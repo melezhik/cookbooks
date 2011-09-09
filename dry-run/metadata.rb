456a4235a91da89023fac393f35188d591655ffe
Alexey <melezhik@gmail.com>
2011-09-09 12:33:27 +0400

    versions bump

--- dry-run/metadata.rb
+++ dry-run/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "melezhik@gmail.com"
 license          "Apache 2.0"
 description      "dry runnable recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 %w{ ubuntu gentoo }.each do |os|
   supports os
--- local_lib/metadata.rb
+++ local_lib/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "CPAN modules provider"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.37"
+version          "0.0.38"
 
