5d77fa6a1698e535f9add6eec956a43ad3b7f769
Alexey <melezhik@gmail.com>
2011-11-08 15:05:59 +0400

    support add

--- catalyst/metadata.rb
+++ catalyst/metadata.rb
@@ -4,3 +4,6 @@ license          "All rights reserved"
 description      "catalyst application resource provider (LWRP)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+%w{ ubuntu gentoo }.each do |os|
+  supports os
+end
