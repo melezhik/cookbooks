f8b5098e1bd094248723b54e3c05dd5aa21d4e0c
Alexey <melezhik@gmail.com>
2011-11-08 15:03:42 +0400

    doc update

--- catalyst/metadata.rb
+++ catalyst/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
-description      "Installs/Configures catalyst application"
+description      "catalyst application resource provider (LWRP)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
