2466362e7951c80062e264890e2dfd695a1c18f7
Alexey <melezhik@gmail.com>
2011-11-08 15:33:37 +0400

    setup version

--- catalyst/metadata.rb
+++ catalyst/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "catalyst application resource provider (LWRP)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 %w{ ubuntu gentoo }.each do |os|
   supports os
 end
