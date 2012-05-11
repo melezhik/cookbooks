890fb3c9427f73c39ae9e0977b128a2a70c67990
Alexey Melezhik <melezhik@gmail.com>
2012-05-11 11:15:36 +0400

    delete old files

--- catalyst-fastcgi/CHANGES.md
+++ catalyst-fastcgi/CHANGES.md
@@ -1,6 +1,11 @@
+# 0.0.4
+- replace rdoc by markdown document
+
+# 0.0.3
+- proper default proc manager
+
 # 0.0.2
 - support for ssl
 - new attributes : start_service, server_alias, apache logs
 
-# 0.0.3
-- proper default proc manager
+
--- catalyst-fastcgi/metadata.rb
+++ catalyst-fastcgi/metadata.rb
@@ -2,8 +2,8 @@ maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "1) Configures catalyst as fastcgi server 2) Configure apache virtual host for it"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.4"
 depends          "catalyst", ">= 0.0.4"
 depends          "apache" , ">= 0.0.3"
 
