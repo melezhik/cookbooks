0e7b2d563f989b8991bc3fe081a5e2bff488b4a0
Alexey <melezhik@gmail.com>
2013-03-22 15:31:57 +0400

    jitty

--- psgi/features/steps/shef.rb
+++ psgi/features/steps/shef.rb
@@ -1,20 +1,21 @@
 Given /^I have chef recipe:$/ do |resource|
  resource.gsub! "'", '"'
- `(echo 'recipe'; echo '#{resource}'; echo 'run_chef' ) > #{cache_dir}/shef.cmd`
+ `(echo 'recipe_mode'; echo '#{resource}'; echo 'run_chef' ) > #{cache_dir}/shef.cmd`
 end
 
 Given /^I run chef recipe on my node$/ do
  cmd = "cat #{cache_dir}/shef.cmd | "
   
  if before_cmd.nil? 
-    cmd << "shef -z -c #{ENV['chef_config']}" 
+    cmd << "bash -c 'shef -z -c #{ENV['chef_config']}' 2>error.log 1>info.log" 
  else
-    cmd << "(#{before_cmd}; shef -z #{ENV['chef_config']})"
+    cmd << "bash -c '(#{before_cmd}; shef -z #{ENV['chef_config']})' 2>error.log 1>info.log"
  end
  
  puts cmd if ENV['debug']
 
  step "I run '#{cmd}'"
+
 end
 
 Given /^I run command (.*) on my node$/ do |cmd|
