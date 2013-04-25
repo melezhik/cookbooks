047640b3727864a30a0369dec677e39bafe9311f
Alexey <melezhik@gmail.com>
2013-04-25 15:35:10 +0400

    fix

--- cpan/recipes/bootstrap.rb
+++ cpan/recipes/bootstrap.rb
@@ -3,8 +3,8 @@ package 'curl'
 execute 'curl -L http://cpanmin.us | perl - --sudo App::cpanminus'
 node.cpan_client.bootstrap.deps.each  do |m|
     execute "cpanm #{m}" do
-    user root
-    group root
+    user 'root'
+    group 'root'
   end
 end
 
