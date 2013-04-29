c2d23ec3c863ce181e5f79f4123e5b355a9fe3aa
Alexey <melezhik@gmail.com>
2013-04-29 14:14:24 +0400

    new structure

--- psgi/test/files/default/tests/minitest/install-psgi-app_test.rb
+++ psgi/test/files/default/tests/minitest/install-psgi-app_test.rb
@@ -1,18 +1,13 @@
-require File.expand_path('../support/helpers', __FILE__)
-
-describe 'psgi_application action install' do
-
-  include Helpers::Test
-
-  it 'creates init script file' do
-    file("/tmp/psgi/app").must_exist
+class PsgiSpec < MiniTest::Chef::Spec
+  describe 'psgi_application action install' do
+    it 'creates init script file' do
+      file("/tmp/psgi/app").must_exist
+    end
+    # Example spec tests can be found at http://git.io/Fahwsw
+    # it 'install init script' { file("/tmp/psgi/app").must_exist }
+    it 'add proper content into init script file' do
+      file("/tmp/psgi/app").must_include 'APPLICATION_USER=user'
+      file("/tmp/psgi/app").must_include 'CATALYST_DEBUG'
+    end
   end
-
-  # Example spec tests can be found at http://git.io/Fahwsw
-  # it 'install init script' { file("/tmp/psgi/app").must_exist }
-  it 'add proper content into init script file' do
-     file("/tmp/psgi/app").must_match /APPLICATION_USER=user/
-  end
-
 end
-
--- psgi/test/specs/install-psgi-app_spec.rb
+++
@@ -1,5 +0,0 @@
-class PsgiSpec < MiniTest::Chef::Spec
-  it "installs init script file" do
-    file("/tmp/psgi/app").must_exists
-  end
-end
