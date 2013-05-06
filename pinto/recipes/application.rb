6088ece1089d81fea105ba6f74884a4f9f92eaca
Alexey <melezhik@gmail.com>
2013-05-06 15:31:04 +0400

    add dep

--- pinto/recipes/application.rb
+++ pinto/recipes/application.rb
@@ -1,11 +1,4 @@
 # installs pinto application - see https://metacpan.org/module/THALJEF/Pinto-0.082/lib/Pinto/Manual/Installing.pod#___pod
-
-
-node.pinto.bootstrap.packages.each do |p|
-    package p
-end
-
-execute 'curl -L http://cpanmin.us | perl - --sudo App::cpanminus'
-
+include_recipe 'cpanminus'
 execute 'cpanm App::Pinto --sudo'
 
