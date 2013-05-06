4cdce5e81d45d8901b0af4ec0a8096ce1de6f58c
Alexey <melezhik@gmail.com>
2013-05-06 17:28:50 +0400

    attributes

--- cpanminus/attributes/default.rb
+++ cpanminus/attributes/default.rb
@@ -1,7 +1,6 @@
+default.cpanminus.bootstrap.download_url = 'http://cpanmin.us'
 default.cpanminus.bootstrap.packages = %w[ curl ]
-
 case platform 
     when 'centos'
         default.cpanminus.bootstrap.packages << 'perl-devel'
 end
-
--- cpanminus/recipes/default.rb
+++ cpanminus/recipes/default.rb
@@ -1,16 +1,6 @@
-#
-# Cookbook Name:: cpanminus
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 node.cpanminus.bootstrap.packages.each do |p|
     package p
 end
-
-execute 'curl -L http://cpanmin.us | perl - --sudo App::cpanminus'
+execute "curl -L #{node.cpanminus.bootstrap.download_url} | perl - --sudo App::cpanminus"
 
 
