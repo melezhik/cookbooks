0ba82d1fd364ac56acc618c1eed8d21e459af532
Alexey <melezhik@gmail.com>
2013-05-07 16:17:27 +0400

    ubuntu

--- pinto/metadata.rb
+++ pinto/metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-%w{ debian centos }.each do |os|
+%w{ ubuntu debian centos }.each do |os|
   supports os
 end
 
