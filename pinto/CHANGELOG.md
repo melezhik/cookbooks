9624ebfced0c6b21f42c0e0d430b8820a3ab8bbc
Alexey <melezhik@gmail.com>
2013-05-08 11:37:28 +0400

    new version

--- pinto/CHANGELOG.md
+++ pinto/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of pinto.
 
+## 0.1.2:
+* pinto bashrc fixed
+* mini::test chef tests imporved
+
 ## 0.1.1:
 * pinto::application - code from https://raw.github.com/thaljef/Pinto/master/etc/install.sh used as prototype
 
--- pinto/files/default/tests/minitest/application_test.rb
+++ pinto/files/default/tests/minitest/application_test.rb
@@ -7,13 +7,11 @@ class PintoSpec < MiniTest::Chef::Spec
             directory("#{node[:pinto][:bootstrap][:home]}").must_exist.with(:group, "#{node[:pinto][:bootstrap][:group]}")
         end
 
-        it "installs pinto client into pinto home" do
-            file("#{node[:pinto][:bootstrap][:home]}/bin/pinto").must_exist.with(:owner, "#{node[:pinto][:bootstrap][:user]}")
-            file("#{node[:pinto][:bootstrap][:home]}/bin/pinto").must_exist.with(:group, "#{node[:pinto][:bootstrap][:group]}")
-            file("#{node[:pinto][:bootstrap][:home]}/bin/pinto").must_have(:mode, "555")
-            result = assert_sh("export PERL5LIB=$PERL5LIB:#{node[:pinto][:bootstrap][:home]}/lib/perl5 && cd #{node[:pinto][:bootstrap][:home]}/bin/ && ./pinto version")
-            %w( App::Pinto Pinto Pinto::Remote).each do |l|
-                assert_includes result, l
+        %w( pinto pintod ).each do |file|
+            it "installs #{file} into pinto home" do
+                file("#{node[:pinto][:bootstrap][:home]}/bin/#{file}").must_exist.with(:owner, "#{node[:pinto][:bootstrap][:user]}")
+                file("#{node[:pinto][:bootstrap][:home]}/bin/#{file}").must_exist.with(:group, "#{node[:pinto][:bootstrap][:group]}")
+                file("#{node[:pinto][:bootstrap][:home]}/bin/#{file}").must_have(:mode, "555")
             end
         end
 
@@ -25,18 +23,33 @@ class PintoSpec < MiniTest::Chef::Spec
             file("#{node[:pinto][:bootstrap][:home]}/etc/bashrc").must_include(node[:fqdn])
         end
 
-        it "smoke tests on installed pinto client" do
-            assert_sh 'rm -rf /tmp/pinto-smoke-repo'
-            assert_sh "mkdir  /tmp/pinto-smoke-repo"
-            assert_sh "export PERL5LIB=$PERL5LIB:#{node[:pinto][:bootstrap][:home]}/lib/perl5 && cd #{node[:pinto][:bootstrap][:home]}/bin/ && ./pinto -r /tmp/pinto-smoke-repo init"
-            assert_sh "export PERL5LIB=$PERL5LIB:#{node[:pinto][:bootstrap][:home]}/lib/perl5 && cd #{node[:pinto][:bootstrap][:home]}/bin/ && ./pinto -r /tmp/pinto-smoke-repo pull Bundler"
-            if node[:pinto][:bootstrap][:slow_tests] == 1
-                result = assert_sh "export PERL5LIB=$PERL5LIB:#{node[:pinto][:bootstrap][:home]}/lib/perl5 && cd #{node[:pinto][:bootstrap][:home]}/bin/ && ./pinto -r /tmp/pinto-smoke-repo list"
-                assert_includes result, '[rf-] Bundler'
+        it "installs valid pinto bashrc file" do
+
+            assert_sh "source #{node[:pinto][:bootstrap][:home]}/etc/bashrc"
+
+            %w( pinto pintod ).each do |file|
+                result = assert_sh "source #{node[:pinto][:bootstrap][:home]}/etc/bashrc && which #{file}"
+                assert_includes result, "#{node[:pinto][:bootstrap][:home]}/bin/#{file}"
+            end
+
+            result = assert_sh("source #{node[:pinto][:bootstrap][:home]}/etc/bashrc && pinto version")
+            %w( App::Pinto Pinto Pinto::Remote ).each do |l|
+                assert_includes result, l
             end
         end
 
 
-    end    
+        it "smoke tests on installed pinto client" do
+            assert_sh 'rm -rf /tmp/pinto-smoke-repo'
+            assert_sh "mkdir  /tmp/pinto-smoke-repo"
+            assert_sh "source #{node[:pinto][:bootstrap][:home]}/etc/bashrc && pinto -r /tmp/pinto-smoke-repo init"
+            assert_sh "source #{node[:pinto][:bootstrap][:home]}/etc/bashrc && pinto -r /tmp/pinto-smoke-repo pull Bundler"
+            if node[:pinto][:bootstrap][:slow_tests] == 1
+                result = assert_sh "source #{node[:pinto][:bootstrap][:home]}/etc/bashrc && pinto -r /tmp/pinto-smoke-repo list"
+                assert_includes result, '[rf-] Bundler'
+            end
+        end
 
+    end    
 end
+
