dde74c1d2d44365675526ce6c99d813ccf166c94
Cepnoy <g.rusakov@adriver.ru>
2013-05-14 11:28:06 +0400

    fix 200 ok output

--- psgi/test/files/default/tests/minitest/run-psgi-app_test.rb
+++ psgi/test/files/default/tests/minitest/run-psgi-app_test.rb
@@ -1,19 +1,19 @@
 class PsgiSpec < MiniTest::Chef::Spec
   describe 'psgi_application action install' do
     it 'creates init script file' do
-      file("/etc/init.d/test").must_exist
+      file("/etc/init.d/app").must_exist
     end
 
     it 'creates init script file with proper owner' do
-      file("/etc/init.d/test").must_have(:owner,"root")
+      file("/etc/init.d/app").must_have(:owner,"root")
     end
 
     it 'creates init script file with proper group' do
-      file("/etc/init.d/test").must_have(:group,"root")
+      file("/etc/init.d/app").must_have(:group,"root")
     end
 
     it 'creates init script file with proper mode' do
-      file("/etc/init.d/test").must_have(:mode,"755")
+      file("/etc/init.d/app").must_have(:mode,"755")
     end
 
     it 'init script returns successfull status' do
@@ -23,10 +23,10 @@ class PsgiSpec < MiniTest::Chef::Spec
 
     it 'CGI script returns 200 OK and Hello World' do
       result = assert_sh("sudo bash -c 'cd /tmp/app && SERVER_PORT=80 SERVER_NAME=127.0.0.1 SCRIPT_NAME=/ REQUEST_METHOD=GET plackup -s CGI app.psgi'")
-      assert_includes result, '200 OK'
+      assert_includes result, 'Status: 200'
       assert_includes result, 'Hello World'
     end
-    it 'service app is up'do
+    it 'app is running'do
       service("app").must_be_running
     end
   end
