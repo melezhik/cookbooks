bf33075fb2ed8deb587af4b4c1c77636edf186fa
Alexey <melezhik@gmail.com>
2013-08-06 12:17:51 +0400

    bump version

--- cpan/metadata.rb
+++ cpan/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "melezhik@gmail.com"
 license          "All rights reserved"
 description      "CPAN modules provider (cpan_client provider LWRP)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.28"
+version          "0.0.29"
 %w{ debian ubuntu centos  gentoo  }.each do |os|
   supports os
 end
