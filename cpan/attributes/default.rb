434ade907a2242d5c1cf1ecc42feb16b7783a878
Alexey <melezhik@gmail.com>
2013-08-06 12:13:36 +0400

    add centos related pakage

--- cpan/attributes/default.rb
+++ cpan/attributes/default.rb
@@ -3,6 +3,7 @@ default.cpan_client.bootstrap.packages = ['curl']
 case platform
 when 'centos'
     default.cpan_client.bootstrap.packages << 'perl-devel'
+    default.cpan_client.bootstrap.packages << 'perl-CPAN'
 end
 
 default.cpan_client.bootstrap.cpan_packages = ['Time::HiRes', 'CPAN', 'local::lib', 'App::pmuninstall']
